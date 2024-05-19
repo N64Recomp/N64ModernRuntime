3504a4c71fe89ad32c7bdc8d910f083fd0d7d9c3
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2024-05-19 15:04:28 -0400

    Added ultramodern and librecomp from Zelda64Recomp, added rt64 submodule, added README

---
+++ librecomp/include/disable_warnings.h
@@ -0,0 +1,4 @@
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
+#endif
---
+++ librecomp/include/recomp_game.h
@@ -0,0 +1,48 @@
+#ifndef __RECOMP_GAME__
+#define __RECOMP_GAME__
+
+#include <vector>
+#include <filesystem>
+
+#include "recomp.h"
+#include "../ultramodern/ultramodern.hpp"
+#include "rt64_layer.h"
+
+namespace recomp {
+	// TODO refactor this to allow a particular project to specify games.
+	// A register game function should be added which takes:
+	//  * Expected ROM hash
+	//  * Internal ROM name (for error messages)
+	//  * Entrypoint function to run for the game
+	//  * Shader cache (optional)
+	// The register game function should return a handle struct (probably containing an ID) so the project can check
+	// which game is running (if any) by querying it.
+	enum class Game {
+		OoT,
+		MM,
+		None,
+		Quit
+	};
+	enum class RomValidationError {
+		Good,
+		FailedToOpen,
+		NotARom,
+		IncorrectRom,
+		NotYet,
+		IncorrectVersion,
+		OtherError
+	};
+	void check_all_stored_roms();
+	bool load_stored_rom(Game game);
+	RomValidationError select_rom(const std::filesystem::path& rom_path, Game game);
+	bool is_rom_valid(Game game);
+	bool is_rom_loaded();
+	void set_rom_contents(std::vector<uint8_t>&& new_rom);
+	void do_rom_read(uint8_t* rdram, gpr ram_address, uint32_t physical_addr, size_t num_bytes);
+	void do_rom_pio(uint8_t* rdram, gpr ram_address, uint32_t physical_addr);
+	void start(ultramodern::WindowHandle window_handle, const ultramodern::audio_callbacks_t& audio_callbacks, const ultramodern::input_callbacks_t& input_callbacks, const ultramodern::gfx_callbacks_t& gfx_callbacks);
+	void start_game(Game game);
+	void message_box(const char* message);
+}
+
+#endif

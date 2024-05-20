284904268ff7ddb8b0e37cc9032993f11b5978d5
Angie <angheloalf95@gmail.com>
2024-05-19 20:48:34 -0400

    4 spaces

--- ultramodern/include/ultramodern/recomp_ui.h
+++ ultramodern/include/ultramodern/recomp_ui.h
@@ -1,15 +1,12 @@
 #ifndef __ULTRAMODERN_RECOMP_UI__
 #define __ULTRAMODERN_RECOMP_UI__
 
-#include <memory>
-#include <string>
-
 namespace recomp {
-	// Currently those functions are expected to be provided by the consumer of this library.
-	// TODO: Change these functions to a callback registering system
+    // Currently those functions are expected to be provided by the consumer of this library.
+    // TODO: Change these functions to a callback registering system
 
-	void destroy_ui();
-	void update_supported_options();
+    void destroy_ui();
+    void update_supported_options();
 }
 
 #endif

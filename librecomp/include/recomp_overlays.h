9c3da3844a814f69e7f5320ca311ea29ca71b1c5
angie <angheloalf95@gmail.com>
2024-05-23 18:00:08 -0400

    Allow registering sections and overlays

--- librecomp/include/recomp_overlays.h
+++ librecomp/include/recomp_overlays.h
@@ -4,9 +4,25 @@
 #include <cstdint>
 #include "sections.h"
 
-extern "C" SectionTableEntry* get_section_table();
-extern "C" size_t get_num_sections();
-extern "C" int* get_overlay_sections_by_index();
+namespace recomp {
+    namespace overlays {
+        struct section_table_data_t {
+            SectionTableEntry* code_sections;
+            size_t num_code_sections;
+            size_t total_num_sections;
+        };
+
+        struct overlays_by_index_t {
+            int* table;
+            size_t len;
+        };
+
+        void register_overlays(const section_table_data_t& sections, const overlays_by_index_t& overlays);
+
+        extern section_table_data_t sections_info;
+        extern overlays_by_index_t overlays_info;
+    }
+}
 
 extern "C" void load_overlays(uint32_t rom, int32_t ram_addr, uint32_t size);
 extern "C" void unload_overlays(int32_t ram_addr, uint32_t size);
--- librecomp/src/patch_loading.cpp
+++ librecomp/src/patch_loading.cpp
@@ -8,5 +8,5 @@
 void load_special_overlay(const SectionTableEntry& section, int32_t ram);
 
 void load_patch_functions() {
-    load_special_overlay(get_section_table()[0], get_section_table()[0].ram_addr);
+    load_special_overlay(recomp::overlays::sections_info.code_sections[0], recomp::overlays::sections_info.code_sections[0].ram_addr);
 }

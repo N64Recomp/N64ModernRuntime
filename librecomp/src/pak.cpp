ca53e8f85a9f31e331e6242b37d469a558ea2d5c
angie <angheloalf95@gmail.com>
2024-05-26 19:30:13 -0400

    Placeholders for the rest

--- librecomp/src/pak.cpp
+++ librecomp/src/pak.cpp
@@ -14,11 +14,27 @@ extern "C" void osPfsInitPak_recomp(uint8_t * rdram, recomp_context* ctx) {
 }
 
 extern "C" void osPfsFreeBlocks_recomp(uint8_t * rdram, recomp_context * ctx) {
-    ctx->r2 = 1; // PFS_ERR_NOPACK
+    PTR(OSPfs) pfs = _arg<0, PTR(OSPfs)>(rdram, ctx);
+    PTR(s32) bytes_not_used = _arg<1, PTR(s32)>(rdram, ctx);
+
+    s32 ret = osPfsFreeBlocks(rdram, pfs, bytes_not_used);
+
+    _return<s32>(ctx, ret);
 }
 
 extern "C" void osPfsAllocateFile_recomp(uint8_t * rdram, recomp_context * ctx) {
-    ctx->r2 = 1; // PFS_ERR_NOPACK
+    PTR(OSPfs) pfs = _arg<0, PTR(OSPfs)>(rdram, ctx);
+    u16 company_code = _arg<1, u16>(rdram, ctx);
+    u32 game_code = _arg<2, u32>(rdram, ctx);
+    PTR(u8) game_name = _arg<3, PTR(u8)>(rdram, ctx);
+    // TODO
+    PTR(u8) ext_name = 0; // _arg<4, PTR(u8)>(rdram, ctx);
+    int file_size_in_bytes = 0; // _arg<5, int>(rdram, ctx);
+    PTR(s32) file_no = 0; // _arg<6, s32>(rdram, ctx);
+
+    s32 ret = osPfsAllocateFile(rdram, pfs, company_code, game_code, game_name, ext_name, file_size_in_bytes, file_no);
+
+    _return<s32>(ctx, ret);
 }
 
 extern "C" void osPfsDeleteFile_recomp(uint8_t * rdram, recomp_context * ctx) {
@@ -38,5 +54,9 @@ extern "C" void osPfsReadWriteFile_recomp(uint8_t * rdram, recomp_context * ctx)
 }
 
 extern "C" void osPfsChecker_recomp(uint8_t * rdram, recomp_context * ctx) {
-    ctx->r2 = 1; // PFS_ERR_NOPACK
+    PTR(OSPfs) pfs = _arg<0, PTR(OSPfs)>(rdram, ctx);
+
+    s32 ret = osPfsChecker(rdram, pfs);
+
+    _return<s32>(ctx, ret);
 }
--- ultramodern/src/pak.cpp
+++ ultramodern/src/pak.cpp
@@ -5,32 +5,40 @@ extern "C" s32 osPfsInitPak(RDRAM_ARG PTR(OSMesgQueue) queue, PTR(OSPfs) pfs, in
     return PFS_ERR_NOPACK;
 }
 
-#if 0
-extern "C" void osPfsFreeBlocks_recomp(uint8_t * rdram, recomp_context * ctx) {
-    ctx->r2 = 1; // PFS_ERR_NOPACK
+// osPfsRepairId
+// osPfsInit
+// osPfsReFormat
+
+extern "C" s32 osPfsChecker(RDRAM_ARG PTR(OSPfs) pfs) {
+    return PFS_ERR_NOPACK;
 }
 
-extern "C" void osPfsAllocateFile_recomp(uint8_t * rdram, recomp_context * ctx) {
-    ctx->r2 = 1; // PFS_ERR_NOPACK
+extern "C" s32 osPfsAllocateFile(RDRAM_ARG PTR(OSPfs) pfs, u16 company_code, u32 game_code, PTR(u8) game_name, PTR(u8) ext_name, int file_size_in_bytes, PTR(s32) file_no) {
+    return PFS_ERR_NOPACK;
 }
 
-extern "C" void osPfsDeleteFile_recomp(uint8_t * rdram, recomp_context * ctx) {
-    ctx->r2 = 1; // PFS_ERR_NOPACK
+extern "C" s32 osPfsFindFile(RDRAM_ARG PTR(OSPfs) pfs, u16 company_code, u32 game_code, PTR(u8) game_name, PTR(u8) ext_name, PTR(s32) file_no) {
+    return PFS_ERR_NOPACK;
 }
 
-extern "C" void osPfsFileState_recomp(uint8_t * rdram, recomp_context * ctx) {
-    ctx->r2 = 1; // PFS_ERR_NOPACK
+extern "C" s32 osPfsDeleteFile(RDRAM_ARG PTR(OSPfs) pfs, u16 company_code, u32 game_code, PTR(u8) game_name, PTR(u8) ext_name) {
+    return PFS_ERR_NOPACK;
 }
 
-extern "C" void osPfsFindFile_recomp(uint8_t * rdram, recomp_context * ctx) {
-    ctx->r2 = 1; // PFS_ERR_NOPACK
+extern "C" s32 osPfsReadWriteFile(RDRAM_ARG PTR(OSPfs) pfs, s32 file_no, u8 flag, int offset, int size_in_bytes, PTR(u8) data_buffer) {
+    return PFS_ERR_NOPACK;
 }
 
-extern "C" void osPfsReadWriteFile_recomp(uint8_t * rdram, recomp_context * ctx) {
-    ctx->r2 = 1; // PFS_ERR_NOPACK
+extern "C" s32 osPfsFileState(RDRAM_ARG PTR(OSPfs) pfs, s32 file_no, PTR(OSPfsState) state) {
+    return PFS_ERR_NOPACK;
 }
 
-extern "C" void osPfsChecker_recomp(uint8_t * rdram, recomp_context * ctx) {
-    ctx->r2 = 1; // PFS_ERR_NOPACK
+// osPfsGetLabel
+// osPfsSetLabel
+// osPfsIsPlug
+
+extern "C" s32 osPfsFreeBlocks(RDRAM_ARG PTR(OSPfs) pfs, PTR(s32) bytes_not_used) {
+    return PFS_ERR_NOPACK;
 }
-#endif
+
+// osPfsNumFiles

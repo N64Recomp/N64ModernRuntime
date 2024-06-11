0c1811ca6f8291c6608f1d6626a73e863902ece9
David Chavez <david@dcvz.io>
2024-06-11 20:47:52 +0200

    Fixes for Windows and size_t (#46)

--- librecomp/include/librecomp/overlays.hpp
+++ librecomp/include/librecomp/overlays.hpp
@@ -2,6 +2,7 @@
 #define __RECOMP_OVERLAYS_H__
 
 #include <cstdint>
+#include <cstddef>
 #include "sections.h"
 
 namespace recomp {
@@ -19,7 +20,7 @@ namespace recomp {
 
         void register_overlays(const overlay_section_table_data_t& sections, const overlays_by_index_t& overlays);
 
-        void register_patches(const char* patch_data, std::size_t patch_size, SectionTableEntry* code_sections);
+        void register_patches(const char* patch_data, size_t patch_size, SectionTableEntry* code_sections);
         void read_patch_data(uint8_t* rdram, gpr patch_data_address);
 
         void init_overlays();

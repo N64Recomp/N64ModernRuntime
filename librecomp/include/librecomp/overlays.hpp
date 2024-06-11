063c85f991e20437c9fa578c69f099863a22c6af
dcvz <david@dcvz.io>
2024-06-11 20:08:20 +0200

    Use plain size_t instead

--- librecomp/include/librecomp/overlays.hpp
+++ librecomp/include/librecomp/overlays.hpp
@@ -9,18 +9,18 @@ namespace recomp {
     namespace overlays {
         struct overlay_section_table_data_t {
             SectionTableEntry* code_sections;
-            std::size_t num_code_sections;
-            std::size_t total_num_sections;
+            size_t num_code_sections;
+            size_t total_num_sections;
         };
 
         struct overlays_by_index_t {
             int* table;
-            std::size_t len;
+            size_t len;
         };
 
         void register_overlays(const overlay_section_table_data_t& sections, const overlays_by_index_t& overlays);
 
-        void register_patches(const char* patch_data, std::size_t patch_size, SectionTableEntry* code_sections);
+        void register_patches(const char* patch_data, size_t patch_size, SectionTableEntry* code_sections);
         void read_patch_data(uint8_t* rdram, gpr patch_data_address);
 
         void init_overlays();

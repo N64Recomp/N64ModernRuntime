57ec421d78d04a3b980502d94de169a0548ecfeb
thecozies <79979276+thecozies@users.noreply.github.com>
2024-07-11 12:28:11 -0500

    Added config registry/option files

---
+++ librecomp/include/librecomp/config.hpp
@@ -0,0 +1,17 @@
+#ifndef __RECOMP_CONFIG_H__
+#define __RECOMP_CONFIG_H__
+
+#include <stdlib.h>
+#include <unordered_map>
+#include <string>
+#include <variant>
+#include <mutex>
+#include "json/json.hpp"
+
+// namespace recomp::config
+
+#include "config/ConfigStore.hpp"
+#include "config/ConfigRegistry.hpp"
+#include "config/ConfigOption.hpp"
+
+#endif

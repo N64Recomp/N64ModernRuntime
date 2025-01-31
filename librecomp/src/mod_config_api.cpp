21bea3b0d1e038a2c33a17aa62d56390a26e3731
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2025-01-31 02:29:42 -0500

    Fix config value parsing to allow integral values for double fields

--- librecomp/src/mod_config_api.cpp
+++ librecomp/src/mod_config_api.cpp
@@ -7,6 +7,9 @@ void recomp_get_config_u32(uint8_t* rdram, recomp_context* ctx, size_t mod_index
     if (uint32_t* as_u32 = std::get_if<uint32_t>(&val)) {
         _return(ctx, *as_u32);
     }
+    else if (double* as_double = std::get_if<double>(&val)) {
+        _return(ctx, uint32_t(int32_t(*as_double)));
+    }
     else {
         _return(ctx, uint32_t{0});
     }
@@ -14,7 +17,10 @@ void recomp_get_config_u32(uint8_t* rdram, recomp_context* ctx, size_t mod_index
 
 void recomp_get_config_double(uint8_t* rdram, recomp_context* ctx, size_t mod_index) {
     recomp::mods::ConfigValueVariant val = recomp::mods::get_mod_config_value(mod_index, _arg_string<0>(rdram, ctx));
-    if (double* as_double = std::get_if<double>(&val)) {
+    if (uint32_t* as_u32 = std::get_if<uint32_t>(&val)) {
+        ctx->f0.d = double(*as_u32);
+    }
+    else if (double* as_double = std::get_if<double>(&val)) {
         ctx->f0.d = *as_double;
     }
     else {

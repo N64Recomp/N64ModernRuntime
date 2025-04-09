bb6b3b1645a582b8d9305f221375da9d3563ee60
Wiseguy <68165316+Mr-Wiseguy@users.noreply.github.com>
2025-04-09 01:15:10 -0400

    Add mod API export to get the save file path (#102)

--- librecomp/src/mod_config_api.cpp
+++ librecomp/src/mod_config_api.cpp
@@ -28,27 +28,38 @@ void recomp_get_config_double(uint8_t* rdram, recomp_context* ctx, size_t mod_in
     }
 }
 
+template <typename StringType>
+void return_string(uint8_t* rdram, recomp_context* ctx, const StringType& str) {
+    // Allocate space in the recomp heap to hold the string, including the null terminator.
+    size_t alloc_size = (str.size() + 1 + 15) & ~15;
+    gpr offset = reinterpret_cast<uint8_t*>(recomp::alloc(rdram, alloc_size)) - rdram;
+    gpr addr = offset + 0xFFFFFFFF80000000ULL;
+
+    // Copy the string's data into the allocated memory and null terminate it.
+    for (size_t i = 0; i < str.size(); i++) {
+        MEM_B(i, addr) = str[i];
+    }
+    MEM_B(str.size(), addr) = 0;
+
+    // Return the allocated memory.
+    ctx->r2 = addr;
+}
+
 void recomp_get_config_string(uint8_t* rdram, recomp_context* ctx, size_t mod_index) {
     recomp::mods::ConfigValueVariant val = recomp::mods::get_mod_config_value(mod_index, _arg_string<0>(rdram, ctx));
     if (std::string* as_string = std::get_if<std::string>(&val)) {
-        const std::string& str = *as_string;
-        // Allocate space in the recomp heap to hold the string, including the null terminator.
-        size_t alloc_size = (str.size() + 1 + 15) & ~15;
-        gpr offset = reinterpret_cast<uint8_t*>(recomp::alloc(rdram, alloc_size)) - rdram;
-        gpr addr = offset + 0xFFFFFFFF80000000ULL;
-
-        // Copy the string's data into the allocated memory and null terminate it.
-        for (size_t i = 0; i < str.size(); i++) {
-            MEM_B(i, addr) = str[i];
-        }
-        MEM_B(str.size(), addr) = 0;
-
-        // Return the allocated memory.
-        ctx->r2 = addr;
+        return_string(rdram, ctx, *as_string);
     }
     else {
         _return(ctx, NULLPTR);
     }
+}
+
+void recomp_free_config_string(uint8_t* rdram, recomp_context* ctx) {
+    gpr str_rdram = (gpr)_arg<0, PTR(char)>(rdram, ctx);
+    gpr offset = str_rdram - 0xFFFFFFFF80000000ULL;
+
+    recomp::free(rdram, rdram + offset);
 }
 
 void recomp_get_mod_version(uint8_t* rdram, recomp_context* ctx, size_t mod_index) {
@@ -73,18 +84,18 @@ void recomp_change_save_file(uint8_t* rdram, recomp_context* ctx, size_t mod_ind
     ultramodern::change_save_file(mod_id_u8, name_u8);
 }
 
-void recomp_free_config_string(uint8_t* rdram, recomp_context* ctx) {
-    gpr str_rdram = (gpr)_arg<0, PTR(char)>(rdram, ctx);
-    gpr offset = str_rdram - 0xFFFFFFFF80000000ULL;
+void recomp_get_save_file_path(uint8_t* rdram, recomp_context* ctx) {
+    std::filesystem::path save_file_path = ultramodern::get_save_file_path();
 
-    recomp::free(rdram, rdram + offset);
+    return_string(rdram, ctx, std::filesystem::absolute(save_file_path).u8string());    
 }
 
 void recomp::mods::register_config_exports() {
     recomp::overlays::register_ext_base_export("recomp_get_config_u32", recomp_get_config_u32);
     recomp::overlays::register_ext_base_export("recomp_get_config_double", recomp_get_config_double);
     recomp::overlays::register_ext_base_export("recomp_get_config_string", recomp_get_config_string);
+    recomp::overlays::register_base_export("recomp_free_config_string", recomp_free_config_string);
     recomp::overlays::register_ext_base_export("recomp_get_mod_version", recomp_get_mod_version);
     recomp::overlays::register_ext_base_export("recomp_change_save_file", recomp_change_save_file);
-    recomp::overlays::register_base_export("recomp_free_config_string", recomp_free_config_string);
+    recomp::overlays::register_base_export("recomp_get_save_file_path", recomp_get_save_file_path);
 }
